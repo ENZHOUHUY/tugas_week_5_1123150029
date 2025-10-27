103e572e08f9e8584c6f6847e70dba9c8d959a4b
ENZHOUHUY <1123150029@global.ac.id>
2025-10-27 16:11:47 +0700

    buat stateless widget class home

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,14 @@
 import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyHome());
+}
+
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
